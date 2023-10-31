bdec1a22f997002cad5c52c58ffb72b577992fe1
Melon-10 <17753679317>
2023-10-31 17:59:06 +0800

    提交自定义返回HTMl

--- lianxi/lianxi1.py
+++ lianxi/lianxi1.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI,Header,Form
 from fastapi.responses import JSONResponse,HTMLResponse
+from starlette.responses import FileResponse
 
 app=FastAPI()
 
@@ -13,6 +14,8 @@ def user():
     <title>测试主页</title>
         <body>
         <p style="color:red">Welcome !</p>
+        <a href="https://www.baidu.com">这是一个链接</a>
+	    <br><br>
         <img src="https://auto.youth.cn/xw/202310/W020231026550359809120.jpg" width="800px" height="500px">
         </body>
         </html>
@@ -30,8 +33,14 @@ def user(username=Form(None),password=Form(None)):
                             status_code=200,
                             headers={"token":"98u98hfwehf9823rh"})
 
+    if username==None and password==None:
+        return JSONResponse(content={"code": "-2", "msg": "账号密码不能为空"},status_code=200,)
     else:
         return JSONResponse(content={"code":"-1","msg":"账号或密码错误"})
 
+@app.get("/avatar")
+def user():
+    avatar='./static/111a.png'
+    return FileResponse(avatar)
 if __name__ == '__main__':
     uvicorn.run(app)
--- lianxi/lianxi2.py
+++ lianxi/lianxi2.py
@@ -1,11 +1,13 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
-# 添加首页
-@app.api_route("/login",methods=["get","post","put"])
-def login():
-    return {"msg":"This  is hen dou methods"}
+app=FastAPI()
+template=Jinja2Templates("pages")
+
+@app.get("/")
+def user(req:Request):
+    return template.TemplateResponse("index.html",context={"request":req})
 
 if __name__ == '__main__':
     uvicorn.run(app)
